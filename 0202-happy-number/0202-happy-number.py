6ba757e7709ecb383b37b208bb40ad90f73525af
Rubens Araujo <rubensaraujobo@gmail.com>
2026-07-15 21:36:10 -0300

    Time: 3 ms (50.11%), Space: 12.4 MB (17.12%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -2,8 +2,7 @@ class Solution(object):
     def isHappy(self, n):
         h = set() 
         while n != 1:
-            if n in h:
-                return False
+            if n in h: return False
             h.add(n)
             n = sum(int(digito) ** 2 for digito in str(n))
         return True
